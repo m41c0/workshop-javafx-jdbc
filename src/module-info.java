59bee8a2761230c4f6d71be8d73bf93f1f6d0da9
Maicon Ferreira <m41c0n@outlook.com>
2021-05-29 22:32:28 -0300

    DepartmentService

--- src/module-info.java
+++ src/module-info.java
@@ -4,4 +4,5 @@ module workshopjavafxjdbc {
 	
 	opens application to javafx.graphics, javafx.fxml;
 	opens gui to javafx.graphics, javafx.fxml;
+	opens model.entities to javafx.graphics, javafx.fxml, javafx.base;
 }
